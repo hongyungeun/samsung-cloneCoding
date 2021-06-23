c3f59ca2e641d10b36dfcaea2c10acb6a3e381b5
ghddbsrms <ghddbsrmsz@naver.com>
2021-06-23 23:51:21 +0900

    second-section

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.main-banner-wrap').slick({
+      dots: true,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000
+    })
+    $('.sub-banner-wrap').slick({
+      dots: true,
+      
+    });
+  });
+  
